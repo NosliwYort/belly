70d14ac856e0ee2129f8207034a1d1850e1e5b0d
IdealEthos <idealethos@gmail.com>
2014-07-10 18:43:29 -0400

    Generate new rails app + add dotenv.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Belly::Application.config.secret_key_base = '70de565e614476ddc653f416432832591dd32ee13457bfdd2601111e92eb2d353ec46de9e031900b80343ccc909d7f7e24703b9e5d491341bcc74358cd87dab2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Belly::Application.config.session_store :cookie_store, key: '_belly_session'
