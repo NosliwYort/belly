e4c2e27258f8327fb6d49afa7f6d42c4937cd55f
IdealEthos <idealethos@gmail.com>
2014-07-12 20:14:28 -0400

    Add controllers and actions for pages, users, plans, meals, and dishes.

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,13 @@
+class DishesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,19 @@
+class PlansController < ApplicationController
+  def new
+  end
+
+  def crete
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
